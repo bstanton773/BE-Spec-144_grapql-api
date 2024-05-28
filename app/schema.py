d7562890997c0d878420d67d47d19fb556755e9d
Brian <bstanton773@gmail.com>
2024-05-28 14:48:33 -0500

    AddNewUser Mutation

--- app/schema.py
+++ app/schema.py
@@ -6,13 +6,29 @@ from app.models import User as UserModel, db
 class UserType(SQLAlchemyObjectType):
     class Meta:
         model = UserModel
+        exclude_fields = ("password",)
 
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
 
     def resolve_users(root, info):
         return db.session.execute(db.select(UserModel)).scalars()
-    
 
 
-schema = graphene.Schema(query=Query)
+class AddNewUser(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    user = graphene.Field(UserType)
+
+    def mutate(root, info, username, email, password):
+        new_user = UserModel(username=username, email=email, password=password)
+        return AddNewUser(user=new_user)
+
+
+class Mutation(graphene.ObjectType):
+    add_new_user = AddNewUser.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
