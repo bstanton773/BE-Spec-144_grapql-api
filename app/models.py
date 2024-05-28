d19968c5b468b9eb19dd94f58f7aede8b520c457
Brian <bstanton773@gmail.com>
2024-05-28 12:05:40 -0500

    Add User __init__ to hash password and auto add to db

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from werkzeug.security import generate_password_hash
 
 
 class Base(DeclarativeBase):
@@ -14,3 +15,12 @@ class User(Base):
     username: Mapped[str] = mapped_column(db.String(255))
     email: Mapped[str] = mapped_column(db.String(255))
     password: Mapped[str] = mapped_column(db.String(255))
+
+    def __init__(self, **kwargs):
+        # Call the parent __init__ so everything still works
+        super().__init__(**kwargs)
+        # Set the password field to be the hashed version of the keyword password
+        self.password = generate_password_hash(kwargs.get('password'))
+        # Add the new instance of User to the database
+        db.session.add(self)
+        db.session.commit()
